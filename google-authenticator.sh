168770098f981db8e8709f855410964b06ef3f3d
anatolmales <79677820+anatolmales@users.noreply.github.com>
2024-10-30 11:48:10 +0700

    Update google-authenticator.sh

--- google-authenticator.sh
+++ google-authenticator.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
-
 #install google-authenticator
 sudo apt install libpam-google-authenticator -y
-
 sudo echo 'auth required pam_google_authenticator.so' >> /etc/pam.d/sshd
 #backup
 sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 #change sshd_config
-
 sudo sed -i -e 's/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/g' /etc/ssh/sshd_config
-
 sudo sed -i -e 's/KbdInteractiveAuthentication no/KbdInteractiveAuthentication yes/g' /etc/ssh/sshd_config
-
 sudo grep -q '^PermitRootLogin' /etc/ssh/sshd_config && sed -i 's/^PermitRootLogin.*/PermitRootLogin prohibit-password/' /etc/ssh/sshd_config || sudo echo 'PermitRootLogin prohibit-password' >> /etc/ssh/sshd_config
-
 cd ~
 google-authenticator -t -f -d -w 15 -r 3 -R 60 -C -e 5
 cat ./.google_authenticator
