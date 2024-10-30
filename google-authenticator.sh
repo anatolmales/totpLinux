5dde524f90ba152e16730dc9c4354542bfaacb5c
anatolmales <79677820+anatolmales@users.noreply.github.com>
2024-10-30 13:32:50 +0700

    Update google-authenticator.sh

--- google-authenticator.sh
+++ google-authenticator.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
-#install google-authenticator
+# Установка google-authenticator
 sudo apt install libpam-google-authenticator -y
-sudo echo 'auth required pam_google_authenticator.so' >> /etc/pam.d/sshd
-#backup
+
+# Добавление строки в файл PAM
+echo 'auth required pam_google_authenticator.so' | sudo tee -a /etc/pam.d/sshd > /dev/null
+
+# Резервное копирование конфигурации SSH
 sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
-#change sshd_config
+
+# Изменение sshd_config
 sudo sed -i -e 's/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/g' /etc/ssh/sshd_config
 sudo sed -i -e 's/KbdInteractiveAuthentication no/KbdInteractiveAuthentication yes/g' /etc/ssh/sshd_config
-sudo grep -q '^PermitRootLogin' /etc/ssh/sshd_config && sed -i 's/^PermitRootLogin.*/PermitRootLogin prohibit-password/' /etc/ssh/sshd_config || sudo echo 'PermitRootLogin prohibit-password' >> /etc/ssh/sshd_config
 
+# Замена PermitRootLogin
+if sudo grep -q '^PermitRootLogin' /etc/ssh/sshd_config; then
+    sudo sed -i 's/^PermitRootLogin.*/PermitRootLogin prohibit-password/' /etc/ssh/sshd_config
+else
+    echo 'PermitRootLogin prohibit-password' | sudo tee -a /etc/ssh/sshd_config > /dev/null
+fi
+
+# Запуск google-authenticator
 cd ~
 google-authenticator -t -f -d -w 15 -r 3 -R 60 -C -e 5
+
+# Отображение секретного ключа
 cat ./.google_authenticator
-sudo  systemctl restart sshd
+
+# Перезапуск службы SSH
+sudo systemctl restart sshd
