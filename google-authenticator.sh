79e01ba20c38b644526cd4207adc38f67e1ff06b
anatolmales <79677820+anatolmales@users.noreply.github.com>
2024-10-30 09:58:55 +0700

    Update google-authenticator.sh

--- google-authenticator.sh
+++ google-authenticator.sh
@@ -7,6 +7,6 @@ sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 sudo sed -i -e 's/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/g' /etc/ssh/sshd_config
 sudo sed -i -e 's/KbdInteractiveAuthentication no/KbdInteractiveAuthentication yes/g' /etc/ssh/sshd_config
 sudo grep -q '^PermitRootLogin' /etc/ssh/sshd_config && sed -i 's/^PermitRootLogin.*/PermitRootLogin prohibit-password/' /etc/ssh/sshd_config || sudo echo 'PermitRootLogin prohibit-password' >> /etc/ssh/sshd_config
-
+cd ~
 google-authenticator -t -f -d -w 3 -e 10 -r 3 -R 30
 sudo  systemctl restart sshd
