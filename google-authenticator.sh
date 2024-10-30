f3e9d4d6a0fc9abaaab49ccc554e55e08b2e0ec4
anatolmales <79677820+anatolmales@users.noreply.github.com>
2024-10-30 13:28:03 +0700

    Update google-authenticator.sh

--- google-authenticator.sh
+++ google-authenticator.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Проверка прав суперпользователя
-if [ "$EUID" -ne 0 ]; then
-    echo "Пожалуйста, запустите скрипт с правами суперпользователя"
-    exec sudo "$0" "$@"  # Перезапускает скрипт с sudo
-    exit
-fi
 #install google-authenticator
 sudo apt install libpam-google-authenticator -y
 sudo echo 'auth required pam_google_authenticator.so' >> /etc/pam.d/sshd
@@ -15,6 +9,7 @@ sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 sudo sed -i -e 's/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/g' /etc/ssh/sshd_config
 sudo sed -i -e 's/KbdInteractiveAuthentication no/KbdInteractiveAuthentication yes/g' /etc/ssh/sshd_config
 sudo grep -q '^PermitRootLogin' /etc/ssh/sshd_config && sed -i 's/^PermitRootLogin.*/PermitRootLogin prohibit-password/' /etc/ssh/sshd_config || sudo echo 'PermitRootLogin prohibit-password' >> /etc/ssh/sshd_config
+
 cd ~
 google-authenticator -t -f -d -w 15 -r 3 -R 60 -C -e 5
 cat ./.google_authenticator
